60bf9643b3a28a0ab68f8e87526ff9eaab987d91
rssilva <rafaelspdasilva@gmail.com>
2013-05-05 18:06:58 -0300

    changing

--- assets/themes/hack-thursday-theme-1/js/customized.js
+++ assets/themes/hack-thursday-theme-1/js/customized.js
@@ -1,1 +1,34 @@
-console.log('customized')
+var Page = function () {
+	return {
+		init: function () {
+			this.setAgenda();
+		},
+
+		setAgenda: function () {
+			this.getAgenda()
+		},
+
+		getAgenda: function () {
+			$.ajax({
+		        type: 'GET',
+		        url: '/calendar-proposal.md',
+		        dataType: 'text',   
+		        success: function(data){
+		       		console.log(data);
+		        },
+		        error: function (data) {
+		        	console.log('error', data);
+		        }
+    		});
+		},
+
+		bindEvents: function () {
+
+		}
+	}
+}
+
+$(document).ready(function () {
+	var page = new Page();
+	page.init();
+});
